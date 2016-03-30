ce29c6e1bf0bd2521a37ea72f81ee35f04619f6a
Rene Christen <accounts@renechristen.net>
2016-03-30 11:49:08 +1100

    first commit

---
+++ src/spinFirefly.cpp
@@ -0,0 +1,450 @@
+#include "spinFirefly.h"
+#include "cinder/ImageIo.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+namespace coc{
+	
+using namespace FlyCapture2;
+
+std::mutex	imageGrabberMutex;
+
+void onImageGrabbed(Image* pImage, const void* pCallbackData)
+{
+
+	imageGrabberMutex.lock();
+	spin::Firefly* firefly = (spin::Firefly*) pCallbackData;
+	firefly->update(pImage);
+	imageGrabberMutex.unlock();
+}
+
+bool Firefly::setup( bool _isCol )
+{    
+	isThreaded = false;
+    isCol = _isCol;
+    width  = 640;
+    height = 480;
+    if (!isCol) {
+		channel = Channel(width,height);
+	} else {
+		surface = Surface( width, height, false, SurfaceChannelOrder::RGB );
+	}
+
+    PrintBuildInfo();
+
+    BusManager busMgr;
+    unsigned int numCameras;
+    error = busMgr.GetNumOfCameras(&numCameras);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    printf( "Number of cameras detected: %u\n", numCameras );
+
+    for (unsigned int i=0; i < 1; i++) //setup first camera only
+    {
+        PGRGuid guid;
+        error = busMgr.GetCameraFromIndex(i, &guid);
+
+		console() <<" GUID is "<< guid.value << endl;
+        if (error != PGRERROR_OK) PrintError( error );
+
+       setupSingleCamera( guid );
+        
+       return true; 
+    }
+	
+    return false;
+}
+
+void Firefly::setup( int _serial, int _w, int _h, bool _isCol )
+{
+	isThreaded = false;
+	isCol = _isCol;
+    width  = _w;
+    height = _h;
+    if (!isCol) {
+		channel = Channel(width,height);
+	} else {
+		surface = Surface( width, height, false, SurfaceChannelOrder::RGB );
+	}
+
+
+    PrintBuildInfo();
+
+    BusManager busMgr;
+    unsigned int numCameras;
+    error = busMgr.GetNumOfCameras(&numCameras);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    printf( "Number of cameras detected: %u:\n", numCameras );
+
+	for (int i=0; i<numCameras; i++) {
+		unsigned int num;
+		busMgr.GetCameraSerialNumberFromIndex(i, &num);
+		console() << "\t" << num << endl;
+	}
+	
+	PGRGuid guid;
+	error = busMgr.GetCameraFromSerialNumber(_serial, &guid);
+
+    if (error != PGRERROR_OK) PrintError( error );
+
+    setupSingleCamera( guid );
+
+}
+
+void Firefly::setupThreaded( int _serial, int _w, int _h, bool _isCol )
+{
+	isThreaded = true;
+	isCol = _isCol;
+    width  = _w;
+    height = _h;
+    if (!isCol) {
+		channel = Channel(width,height);
+	} else {
+		surface = Surface( width, height, false, SurfaceChannelOrder::RGB );
+	}
+
+
+    PrintBuildInfo();
+
+    BusManager busMgr;
+    unsigned int numCameras;
+    error = busMgr.GetNumOfCameras(&numCameras);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    printf( "Number of cameras detected: %u:\n", numCameras );
+
+	for (int i=0; i<numCameras; i++) {
+		unsigned int num;
+		busMgr.GetCameraSerialNumberFromIndex(i, &num);
+		console() << "\t" << num << endl;
+	}
+	
+	PGRGuid guid;
+	error = busMgr.GetCameraFromSerialNumber(_serial, &guid);
+
+    if (error != PGRERROR_OK) PrintError( error );
+
+    // Connect to a camera
+    error = cam.Connect(&guid);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    // Get the camera information
+    CameraInfo camInfo;
+    error = cam.GetCameraInfo(&camInfo);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    PrintCameraInfo(&camInfo);   
+
+    // Start capturing images
+	error = cam.StartCapture(onImageGrabbed, (void*)this);
+    if (error != PGRERROR_OK) PrintError( error );
+
+}
+
+void Firefly::setup( int _serial, bool _isCol, Format7ImageSettings _fmt7ImageSettings, float _speed, bool _threaded )
+{
+	isThreaded = _threaded;
+	isCol = _isCol;
+    width  = _fmt7ImageSettings.width;
+    height = _fmt7ImageSettings.height;
+    if (!isCol) {
+		channel = Channel(width,height);
+	} else {
+		surface = Surface( width, height, false, SurfaceChannelOrder::RGB );
+	}
+
+    PrintBuildInfo();
+
+    BusManager busMgr;
+    unsigned int numCameras;
+    error = busMgr.GetNumOfCameras(&numCameras);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    printf( "Number of cameras detected: %u:\n", numCameras );
+
+	for (int i=0; i<numCameras; i++) {
+		unsigned int num;
+		busMgr.GetCameraSerialNumberFromIndex(i, &num);
+		console() << "\t" << num << endl;
+	}
+	
+	PGRGuid guid;
+	error = busMgr.GetCameraFromSerialNumber(_serial, &guid);
+
+    if (error != PGRERROR_OK) PrintError( error );
+
+    // Connect to a camera
+    error = cam.Connect(&guid);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    // Get the camera information
+    CameraInfo camInfo;
+    error = cam.GetCameraInfo(&camInfo);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    PrintCameraInfo(&camInfo); 
+
+	cam.SetFormat7Configuration( &_fmt7ImageSettings, _speed);
+
+	if (!_threaded) {
+		error = cam.StartCapture();
+	} else {
+		error = cam.StartCapture(onImageGrabbed, (void*)this);
+	}
+	if (error != PGRERROR_OK) PrintError( error );
+
+}
+
+void Firefly::setupSingleCamera( PGRGuid guid )
+{
+
+	 // Connect to a camera
+    error = cam.Connect(&guid);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    // Get the camera information
+    CameraInfo camInfo;
+    error = cam.GetCameraInfo(&camInfo);
+    if (error != PGRERROR_OK) PrintError( error );
+
+    PrintCameraInfo(&camInfo);   
+
+    // Start capturing images
+    error = cam.StartCapture();
+
+	//error = cam.StartCapture(onImageGrabbed, (void*)this);
+    if (error != PGRERROR_OK) PrintError( error );
+
+	
+}
+
+void Firefly::update() {
+
+		if (isThreaded)
+		{
+			console() << "Error: Threaded, no need to call update()!" <<endl;
+		}
+		else 
+		{
+			update( nullptr );
+		}
+
+  //      // Retrieve an image
+  //      Image rawImage; 
+		//error = cam.RetrieveBuffer( &rawImage );
+		//if (error != PGRERROR_OK)PrintError( error );            
+
+		//		
+		//Image convertedImage;
+		//if (!isCol) {//make mono
+		//	error = rawImage.Convert( PIXEL_FORMAT_MONO8, &convertedImage );    
+		//	if (error != PGRERROR_OK) PrintError( error );
+		//	memcpy(channel.getData(), convertedImage.GetData(), convertedImage.GetCols() * convertedImage.GetRows());
+		//	isNewFrame = true;
+		//}
+		//else {//RGB
+		//	//convertedImage.SetDefaultColorProcessing(DEFAULT);
+
+		//	error = rawImage.Convert( PIXEL_FORMAT_RGB8, &convertedImage ); 
+		//	
+		//	if (error != PGRERROR_OK) PrintError( error );
+		//	memcpy(surface.getData(), convertedImage.GetData(), convertedImage.GetCols() * convertedImage.GetRows() * 3);
+		//	isNewFrame = true;
+		//
+		//}
+}
+
+
+void Firefly::update( Image *rawImage ) //threaded function for flea
+{
+
+	if (rawImage == nullptr) {
+		// Retrieve an image
+		rawImage = new Image(); 
+		error = cam.RetrieveBuffer( rawImage );
+		if (error != PGRERROR_OK)PrintError( error ); 
+	}
+	
+    Image convertedImage;
+	if (!isCol) {//make mono
+		error = rawImage->Convert( PIXEL_FORMAT_MONO8, &convertedImage );    
+		if (error != PGRERROR_OK) PrintError( error );
+        // copy to channel
+
+		imageMutex.lock();
+		memcpy(channel.getData(), convertedImage.GetData(), convertedImage.GetCols() * convertedImage.GetRows());
+		isNewFrame = true;
+		imageMutex.unlock();
+
+	}
+	else {//RGB
+		
+		error = rawImage->Convert( PIXEL_FORMAT_RGB, &convertedImage );   
+		if (error != PGRERROR_OK) PrintError( error );
+		
+		imageMutex.lock();
+		memcpy(surface.getData(), convertedImage.GetData(), convertedImage.GetCols() * convertedImage.GetRows() * 3);
+		isNewFrame = true;
+		imageMutex.unlock();
+
+		
+	}
+	
+}
+
+void Firefly::generateTexture() {
+	
+	if (!isCol) {//make mono
+		imageMutex.lock();
+        tex = gl::Texture( channel );
+		imageMutex.unlock();
+	}
+	else {//RGB
+		imageMutex.lock();
+		if (surface) tex = gl::Texture( surface );
+		imageMutex.unlock();
+	}
+}
+
+void Firefly::draw( Vec2f _pos ) 
+{
+	generateTexture();
+    if (tex) gl::draw( tex, _pos );
+}
+
+void Firefly::draw( Rectf _bounds )
+{
+	generateTexture();
+    if (tex) gl::draw( tex, _bounds);
+}
+
+void Firefly::stop() {
+        // Stop capturing images
+    error = cam.StopCapture();
+    if (error != PGRERROR_OK) PrintError( error );   
+    // Disconnect the camera
+    error = cam.Disconnect();
+    if (error != PGRERROR_OK) PrintError( error );
+}
+
+void Firefly::printPropertyInfo( PropertyType _type, string _name) {
+	PropertyInfo propInfo;
+	propInfo.type = _type;
+	cam.GetPropertyInfo( &propInfo );
+	
+	printf(
+		"\nCamera Property Type: %s\n"
+		"\tpresent: %i\n"
+		"\tautoSupported: %i\n"
+		"\tmanualSupported: %i\n"
+		"\tonOffSupported: %i\n"
+		"\tonePushSupported: %i\n"
+		"\tabsValSupported: %i\n"
+		"\treadOutSupported: %i\n"
+		"\tmin: %i\n"
+		"\tmax: %i\n"
+		"\tabsMin: %f\n"
+		"\tabsMax: %f\n\n",
+		_name.c_str(),
+		(int)propInfo.present,
+		(int)propInfo.autoSupported,
+		(int)propInfo.manualSupported,
+		(int)propInfo.onOffSupported,
+		(int)propInfo.onePushSupported,
+		(int)propInfo.absValSupported,
+		(int)propInfo.readOutSupported,
+		propInfo.min,
+		propInfo.max,
+		propInfo.absMin,
+		propInfo.absMax
+		);
+		
+} 
+
+void Firefly::printProperty( PropertyType _type, string _name) {
+	Property propInfo;
+	propInfo.type = _type;
+	cam.GetProperty( &propInfo );
+	
+	printf(
+		"\nCamera Property: %s\n"
+		"\tpresent: %i\n"
+		"\tabsControl: %i\n"
+		"\tonePush: %i\n"
+		"\tonOff: %i\n"
+		"\tautoManualMode: %i\n"
+		"\tvalueA: %i\n"
+		"\tvalueB: %i\n"
+		"\tabsValue: %f\n\n",
+		_name.c_str(),
+		(int)propInfo.present,
+		(int)propInfo.absControl,
+		(int)propInfo.onePush,
+		(int)propInfo.onOff,
+		(int)propInfo.autoManualMode,
+		propInfo.valueA,
+		propInfo.valueB,
+		propInfo.absValue
+		);
+		
+} 
+
+void Firefly::setProperty( PropertyType _type, float _absVal) {
+	Property prop;
+	prop.type = _type;
+	cam.GetProperty( &prop );
+
+	prop.absControl = true;
+	prop.autoManualMode = false;
+	prop.absValue = _absVal;
+	cam.SetProperty( &prop );
+}
+
+void Firefly::PrintBuildInfo()
+{
+    FC2Version fc2Version;
+    Utilities::GetLibraryVersion( &fc2Version );
+    char version[128];
+    sprintf( 
+        version, 
+        "FlyCapture2 library version: %d.%d.%d.%d\n", 
+        fc2Version.major, fc2Version.minor, fc2Version.type, fc2Version.build );
+
+    printf( version );
+
+    char timeStamp[512];
+    sprintf( timeStamp, "Application build date: %s %s\n\n", __DATE__, __TIME__ );
+
+    printf( timeStamp );
+}
+
+void Firefly::PrintCameraInfo( CameraInfo* pCamInfo )
+{
+    printf(
+        "\n*** CAMERA INFORMATION ***\n"
+        "Serial number - %u\n"
+        "Camera model - %s\n"
+        "Camera vendor - %s\n"
+        "Sensor - %s\n"
+        "Resolution - %s\n"
+        "Firmware version - %s\n"
+        "Firmware build time - %s\n\n",
+        pCamInfo->serialNumber,
+        pCamInfo->modelName,
+        pCamInfo->vendorName,
+        pCamInfo->sensorInfo,
+        pCamInfo->sensorResolution,
+        pCamInfo->firmwareVersion,
+        pCamInfo->firmwareBuildTime );
+}
+
+void Firefly::PrintError( Error error )
+{
+    error.PrintErrorTrace();
+}
+
+
+}//namespace coc
---
+++ src/spinFirefly.h
@@ -0,0 +1,112 @@
+/*
+
+cross platform class for using Point Grey firefly cameras
+uses Point Grey SDk on windows and LibDC1394 on OSX
+
+todo: currently only uses colour cams and coverts to mono
+
+ */
+
+
+#pragma once
+
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Surface.h"
+#include "cinder/Thread.h"
+#include "cinder/ConcurrentCircularBuffer.h"
+
+	
+//POINT GREY
+// #if defined(WIN32) || defined(WIN64)
+
+#ifndef _WIN32_WINNT            // Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501     // Change this to the appropriate value to target other versions of Windows.
+#endif          
+
+#define _CRT_SECURE_NO_WARNINGS                                 
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include "FlyCapture2.h"
+
+
+namespace coc {
+
+	class FlyCapture {
+	public:
+
+
+        FlyCapture() : isNewFrame(false) {}
+        bool setup( bool _isCol = false );//todo: test return in windows
+
+
+		void setup( int _serial, int _w, int _h, bool _isCol );
+		void setupThreaded( int _serial, int _w, int _h, bool _isCol );
+		void setup( int _serial, bool _isCol, FlyCapture2::Format7ImageSettings _fmt7ImageSettings, float _speed, bool _threaded );
+
+		bool		isThreaded;
+		std::mutex	imageMutex;
+		ci::Surface surface;
+		
+		void generateTexture();
+		void update();
+		void update( FlyCapture2::Image *rawImage );
+
+		void printPropertyInfo( FlyCapture2::PropertyType _type, std::string _name);
+		void printProperty( FlyCapture2::PropertyType _type, std::string _name);
+		void setProperty( FlyCapture2::PropertyType _type, float _absVal);
+
+
+		void draw( ci::Vec2f _pos = ci::Vec2f(0,0) );
+        void draw( ci::Rectf _bounds );
+		void stop();
+
+		int getWidth() { return width; }
+		int getHeight() { return height; }
+		ci::Channel getChannel() { return channel; }
+		ci::Surface getSurface() { return surface; }
+		unsigned char * getData() {
+			unsigned char * data;
+			isCol ? data = surface.getData() : data = channel.getData() ; 
+			return data;
+		} 
+
+        bool isNewFrame;
+        bool checkNewFrame() {
+            bool tmp = isNewFrame;
+            isNewFrame = false;
+            return tmp;
+        }
+
+
+	private:
+
+        std::string makeString(int name);
+        float       makeFloat( int name);
+
+        ci::Channel                     channel;
+        int                             width;
+        int                             height;
+        ci::gl::Texture                 tex;
+        bool                            isCol;
+        
+
+        FlyCapture2::Camera				cam;
+        FlyCapture2::Image              rawImage;
+        FlyCapture2::Image              convertedImage;
+        FlyCapture2::Error              error;
+
+        void setupSingleCamera( FlyCapture2::PGRGuid guid );
+
+        void PrintBuildInfo();
+        void PrintCameraInfo( FlyCapture2::CameraInfo* pCamInfo );
+        void PrintError( FlyCapture2::Error error );
+
+
+
+	};//class FlyCapture
+
+}//namespace coc
